4cafaeb7a766f024159c6d8ca9d97629ecc21982
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2022-12-11 21:55:46 +0700

    Update discord-font-change.js

--- discord-font-change.js
+++ discord-font-change.js
@@ -8,22 +8,20 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @run-at       document-start
+// @run-at       document-body
 // @antifeature  tracking
 // ==/UserScript==
-window.addEventListener('DOMContentLoaded', async () => {
-    const css = `
-    <style>
-        :root {
-            --font-primary: Segoe UI Regular,sans-serif;
-        }
-        .nameTag-H6kSJ0 {
-            line-height: 1.5;
-        }
-        .discriminator-WV5K5s {
-            line-height: 23px;
-        }
-    </style>
-    `;
-    document.head.insertAdjacentHTML("beforeend", css)
-})
+const css = `
+<style>
+    :root {
+        --font-primary: Segoe UI Regular,sans-serif;
+    }
+    .nameTag-H6kSJ0 {
+        line-height: 1.5;
+    }
+    .discriminator-WV5K5s {
+        line-height: 23px;
+    }
+</style>
+`;
+document.head.insertAdjacentHTML("beforeend", css)
